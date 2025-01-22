380cabb61ef04e7c036b7bf5ceb4c425b87bacf8
hector <webzonne29@gmail.com>
2025-01-22 13:32:53 -0400

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,21 @@ import Egresados from './pages/Egresados';
 import Home from './pages/index';
 import NotRegistered from './components/NotRegistered';
 import Admin from './pages/Admin';
+import { Link } from 'react-router-dom';
 
 export default function App() {
 
   return (
     <Router>
       <div>
-    
+      <nav className='p-4 flex justify-around'>
+      <Link to="/">Home</Link>
+      <Link to="/admin">Admin</Link>
+      <Link to="/f">Fundesurg</Link>
+      <Link to="/c">Control de Estudio</Link>
+      <Link to="/e">Egresados</Link>
+      <Link to="/error">Error</Link>
+    </nav>
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
